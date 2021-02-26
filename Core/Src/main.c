5215be902ca1ddff29822fc86ef2d48d7d0827c0
parnivkiiz801c2 <kulikovrs@mer.ci.nsu.ru>
2021-02-26 11:29:20 +0700

    Our first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,454 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdlib.h"
+#include "stdio.h"
+#include "stm32f4xx_ll_gpio.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define angle_step		183.33
+#define	LIS3DSH_Read	0x8000
+#define	Read 					1
+#define	Write					0
+#define CTRL_REG4			0x20
+#define Out_XL				0x28
+#define Out_XH				0x29
+#define Out_YL				0x2A
+#define Out_YH				0x2B
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+/* USER CODE BEGIN PV */
+volatile int Int_DataX = 0;
+volatile int Int_DataY = 0;
+volatile	uint16_t Out_X, Y_Out = 0x0000;
+volatile float angle_X, Y_angle = 0.0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+/* USER CODE BEGIN PFP */
+uint8_t LIS3DSH_Transmit(uint8_t _Addr, uint8_t _Data, uint8_t _Read);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	char DataX[20] =   {0x00};
+	char DataY[20] =   {0x00};
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+	//uint16_t	TX=0x8F00;
+	//uint16_t	RX=0x0000;
+	//uint8_t		Output_Axel = 0;
+	//0 - zapis'
+	volatile uint8_t DAT = LIS3DSH_Transmit(CTRL_REG4,0x63,Write);
+	if(LIS3DSH_Transmit(CTRL_REG4,0x00,Read) == 0x63)
+	{
+		HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+
+	}
+	else
+	{
+		HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+	}
+	
+	//HAL_GPIO_WritePin(GPIOD, gpio_);
+	//INICIALIZACIYA_LCD
+	//STATE_1
+	HAL_Delay(50);
+	volatile uint32_t tmp = LL_GPIO_ReadOutputPort(GPIOD);
+	tmp &= 0xFF00;
+	tmp |= 0x3C;
+	LL_GPIO_WriteOutputPort(GPIOD, tmp);
+	HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//STATE_2-4
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//STATE_5
+	tmp = LL_GPIO_ReadOutputPort(GPIOD);
+	tmp &= 0xFF00;
+	tmp |= 0x0F;
+	LL_GPIO_WriteOutputPort(GPIOD, tmp);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+	HAL_Delay(1);
+	//STATE_6
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//STATE_7	
+	tmp = LL_GPIO_ReadOutputPort(GPIOD);
+	tmp &= 0xFF00;
+	tmp |= 0x01;
+	LL_GPIO_WriteOutputPort(GPIOD, tmp);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+	HAL_Delay(1);	
+	//STATE_8
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//STATE_9
+	tmp = LL_GPIO_ReadOutputPort(GPIOD);
+	tmp &= 0xFF00;
+	tmp |= 0x06;
+	LL_GPIO_WriteOutputPort(GPIOD, tmp);
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+	HAL_Delay(1);	
+	//STATE_10
+	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//END OF INICIALIZATION LCD
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		HAL_Delay(12);
+		
+    /* USER CODE END WHILE */
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= 0x01;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		Out_X = LIS3DSH_Transmit(Out_XL, 0x00, Read) + (LIS3DSH_Transmit(Out_XH, 0x00, Read) << 8);
+		if ((Out_X & 0x8000) != 0) Int_DataX = (-1 - (0xFFFF - Out_X));
+		else Int_DataX = Out_X;
+		angle_X = Int_DataX / angle_step;
+		Y_Out = LIS3DSH_Transmit(Out_YL, 0x00, Read) + (LIS3DSH_Transmit(Out_YH, 0x00, Read) << 8);
+		if ((Y_Out & 0x8000) != 0) Int_DataY = (-1 - (0xFFFF - Y_Out));
+		else Int_DataY = Y_Out;
+		Y_angle = Int_DataY / angle_step;
+		sprintf(DataX, "%.2f", angle_X);
+		sprintf(DataY, "%.2f", Y_angle);
+		char v = 'X';
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= v;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		v = ':';
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= v;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		for(int i = 0; i < 10; i++)
+		{
+			if(DataX[i] == 0x00) break;
+			tmp = LL_GPIO_ReadOutputPort(GPIOD);
+			tmp &= 0xFF00;
+			tmp |= DataX[i];
+			LL_GPIO_WriteOutputPort(GPIOD, tmp);
+			HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+			HAL_Delay(1);
+			HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+			HAL_Delay(1);
+		}
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= 0xC0;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		v = 'Y';
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= v;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		v = ':';
+		tmp = LL_GPIO_ReadOutputPort(GPIOD);
+		tmp &= 0xFF00;
+		tmp |= v;
+		LL_GPIO_WriteOutputPort(GPIOD, tmp);
+		HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+		HAL_Delay(1);
+		HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+		HAL_Delay(1);
+		for(int i = 0; i < 10; i++)
+		{
+			if(DataY[i] == 0x00) break;
+			tmp = LL_GPIO_ReadOutputPort(GPIOD);
+			tmp &= 0xFF00;
+			tmp |= DataY[i];
+			LL_GPIO_WriteOutputPort(GPIOD, tmp);
+			HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_SET);
+			HAL_Delay(1);
+			HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, GPIO_PIN_RESET);
+			HAL_Delay(1);
+		}
+		HAL_Delay(500);
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, E_Pin|RS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : CS_Pin */
+  GPIO_InitStruct.Pin = CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(CS_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD0 PD1 PD2 PD3
+                           PD4 PD5 PD6 PD7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : E_Pin RS_Pin */
+  GPIO_InitStruct.Pin = E_Pin|RS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+	uint8_t LIS3DSH_Transmit(uint8_t _Addr, uint8_t _Data, uint8_t _Read)
+	{
+		uint16_t RX = 0x0000;
+		uint16_t Data = (_Addr << 8) + _Data;
+		if(_Read != 0) Data += LIS3DSH_Read;
+		HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+		HAL_SPI_TransmitReceive(&hspi1,(uint8_t *) &Data,(uint8_t *)&RX, 1, 10);
+		HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+		return(RX&0xFF);
+	}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ MDK-ARM/Axel_Angle/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+axel_angle\startup_stm32f407xx.o: startup_stm32f407xx.s
